0e055168de45bae92b8b890474612c9e6799518a
Wahid S Contratcor <wcontractor@gmail.com>
2014-12-13 11:43:49 -0500

    Version 2

--- complete.R
+++ complete.R
@@ -3,10 +3,9 @@ complete <- function(directory, id = 1:332) {
   vect1=numeric()
   vect2=numeric()
   for(file in id){
-    x <- read.csv(files_list[file])
-    nobs = nrow(x)
-    vect1 = c(vect1, nobs)
-    vect2 = c(vect2, file)
+    nobs <- sum(complete.cases(read.csv(files_list[file]))) 
+    vect1 <- c(vect1, nobs)
+    vect2 <- c(vect2, file)
   } 
   output <- data.frame(id=vect2, nobs=vect1)
   output
